eece9ca81117d4cc583abe24f4b343115e4b288d
parthiban <parthiban@flendzz.com>
2021-10-26 17:24:15 +0530

    added image field

--- django_aws3_app/models.py
+++ django_aws3_app/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class book_box(models.Model):
     title = models.CharField(max_length=30)
     author = models.CharField(max_length=30)
-    price = models.IntegerField()
-    document = models.FileField(max_length=30)
+    price = models.IntegerField(max_length=10)
+    pic = models.ImageField()
+    document = models.FileField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
